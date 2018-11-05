94c610da507887c21df7a318d891dc29bae40892
Andrey Khataev <andrey.hataev@balance-pl.ru>
2018-11-05 07:26:31 +0300

    parser refactoring

--- modules/parser.js
+++ modules/parser.js
@@ -1,9 +1,16 @@
-// TODO: $ instead of cheerio
-const cheerio = require('cheerio');
+const $ = require('cheerio');
 const { DateTime } = require('luxon');
 const util = require('./util');
 
 let historyManager, settings_global;
+
+function getColumnText(order_row, column_number) {
+  return $(order_row).children('td').eq(column_number).text();
+};
+
+function getOrderNumber(order_row) {
+  return getColumnText(order_row, 1);
+};
 
 function filterOnlyOrders (i, elem) {
   return i > 0;
@@ -17,7 +24,7 @@ function filterByTime (i, elem) {
     return true;
 
   try {
-    let dt_string = cheerio(elem).children('td').eq(2).text();
+    let dt_string = $(elem).children('td').eq(2).text();
     // dt = DateTime.fromFormat(dt_string, 'dd-LL HH:mm');
     let hour = Number.parseInt(dt_string.split(' ')[1].split(':')[0]);
     return hour >= from_hour && hour <= to_hour;
@@ -32,7 +39,7 @@ function filterByStatus (i, elem) {
 };
 
 function filterByHistory (i, elem) {
-  let order_number = cheerio(elem).children('td').eq(1).text();
+  let order_number = getOrderNumber(elem);
 
   return !historyManager.dayHistoryIncludes(order_number);
 };
@@ -49,7 +56,6 @@ seizeOrderUrl = function (orderNumber) {
 };
 
 let parser = function (history_manager, request, settings, logger) {
-  // TODO: how to separate private and public functions?
   historyManager = history_manager;
   settings_global = settings;
 
@@ -65,10 +71,10 @@ let parser = function (history_manager, request, settings, logger) {
         return null;
       }
 
-      let $ = cheerio.load(body);
+      let $$ = $.load(body);
       selector = '#body > table:nth-child(2) > tbody > tr > td > table:nth-child(6) > tbody';
 
-      $orders_tbody = $(selector);
+      $orders_tbody = $$(selector);
       $orders = $orders_tbody.children('tr');
       historyManager.readOrdersHistory(date);
       $orders = $orders.filter(filterOrders);
@@ -78,16 +84,16 @@ let parser = function (history_manager, request, settings, logger) {
   };
 
   this.getColumnText = function (order_row, column_number) {
-    return cheerio(order_row).children('td').eq(column_number).text()
+    return getColumnText(order_row, column_number);
   };
 
   this.getOrderNumber = function(order_row) {
-    this.getColumnText(order_row, 1);
+    return getOrderNumber(order_row);
   };
 
   this.renderOrderData = function (order) {
     // 1, 3, 6, 7, 5, 2
-    let $order = cheerio(order);
+    let $order = $(order);
     let orderNumber = this.getColumnText($order, 1),
       metro = this.getColumnText($order, 3),
       address = this.getColumnText($order, 6),
